b99e14dc0562b0545ac1ef776e49923f6ba74106
hj01857655 <1292548381@qq.com>
2026-04-23 02:11:46 +0800

    feat: implement Tooltip component using Radix UI primitives

--- src/components/ui/tooltip.tsx
+++ src/components/ui/tooltip.tsx
@@ -6,28 +6,28 @@ import { cn } from "@/lib/utils"
 function TooltipProvider({
   delayDuration = 0,
   ...props
-}) {
+}: React.ComponentProps<typeof TooltipPrimitive.Provider>) {
   return (<TooltipPrimitive.Provider data-slot="tooltip-provider" delayDuration={delayDuration} {...props} />);
 }
 
 function Tooltip({
   ...props
-}) {
+}: React.ComponentProps<typeof TooltipPrimitive.Root>) {
   return <TooltipPrimitive.Root data-slot="tooltip" {...props} />;
 }
 
 function TooltipTrigger({
   ...props
-}) {
+}: React.ComponentProps<typeof TooltipPrimitive.Trigger>) {
   return <TooltipPrimitive.Trigger data-slot="tooltip-trigger" {...props} />;
 }
 
 function TooltipContent({
   className,
-  sideOffset = 0,
+  sideOffset = 4,
   children,
   ...props
-}) {
+}: React.ComponentProps<typeof TooltipPrimitive.Content>) {
   return (
     <TooltipPrimitive.Portal>
       <TooltipPrimitive.Content
